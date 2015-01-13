5429301ced555787795a3e61f24e6a5aaf9e6b20
Joshua Miller <josh@joshmiller.io>
2015-01-13 10:11:46 -0800

    Adding client src

--- src/Fondo.jl
+++ src/Fondo.jl
@@ -1,5 +1,19 @@
+import HTTPClient.HTTPC
+import JSON
+
 module Fondo
 
-# package code goes here
+export getValue
 
-end # module
+function getvalue(node, id)
+    response = HTTPC.get(node * "/value/" * id)
+    result = JSON.parse(bytestring(response.body))
+    result["uri"] = replace(result["uri"], " ", "+")
+    result["data"] = bytestring(HTTPC.get(result["uri"]).body)
+    result
+end
+
+function putvalue(node, id)
+end
+
+end
